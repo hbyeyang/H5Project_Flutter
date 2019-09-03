3f10acb27844ec5481a53b77877fb65428b51fe6
叶阳 <hbyeyang@yeah.net>
2019-09-03 14:20:08 +0800

    提交测试版本2

--- lib/page/listpage.dart
+++ lib/page/listpage.dart
@@ -3,6 +3,8 @@ import 'package:demo_test/bean/pageBean.dart';
 import 'package:demo_test/main.dart';
 import 'package:demo_test/util/utils.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ListPage extends StatelessWidget {
 //  final String title;
@@ -17,6 +19,7 @@ class ListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     pageBean = ModalRoute.of(context).settings.arguments;
+    print(pageBean.dataLists.length);
     // TODO: implement build
     List<Widget> titleContent = [];
     titleContent.add(new Text(
@@ -58,7 +61,7 @@ class ListPage extends StatelessWidget {
 //        ),
 //      ),
       body: new ListView.builder(
-          itemExtent: 80.0,
+          itemExtent: 150.0,
           itemCount: pageBean.dataLists.length,
           itemBuilder: buildItem),
     );
@@ -66,12 +69,15 @@ class ListPage extends StatelessWidget {
 
   //ListView的Item 一个有点击事件的Text控件
   Widget buildItem(BuildContext context, int index) {
-    String number = String.fromCharCode(index);
+    var _url = pageBean.dataLists[index]['url'];
+    if (!_url.startsWith("http")) {
+      _url = "http://" + _url;
+    }
     //设置分割线
-    if (index.isOdd) return new Divider();
+//    if (index.isOdd) return new Divider();
     //设置字体样式
     TextStyle textStyle = new TextStyle(
-        fontWeight: FontWeight.bold, fontSize: 18.0, color: Colors.redAccent);
+        fontWeight: FontWeight.bold, fontSize: 17.0, color: Colors.redAccent);
     TextStyle textStyle1 = new TextStyle(
         fontWeight: FontWeight.bold, fontSize: 14.0, color: Colors.blue);
     TextStyle textStyle2 = new TextStyle(
@@ -84,10 +90,12 @@ class ListPage extends StatelessWidget {
       },
       child: new Container(
         alignment: Alignment.center,
+        color: Colors.white70,
 //        padding: const EdgeInsets.all(8.0),
 //        child: new Text(pageBean.dataLists[index]['name'], style: textStyle),
         child: new Column(
           children: <Widget>[
+            Text("编号：" + index.toString()),
             Text("名称：" + pageBean.dataLists[index]['name'],
                 overflow: TextOverflow.ellipsis, style: textStyle),
             Text(
@@ -114,10 +122,58 @@ class ListPage extends StatelessWidget {
                 ),
               ],
             ),
+            new Row(
+              children: <Widget>[
+                Expanded(
+                  child: RaisedButton(
+                    child: Text("外跳"),
+                    onPressed: () {
+                      _launchURL(_url);
+                    },
+                    splashColor: Colors.amberAccent,
+                    color: Colors.greenAccent,
+                    elevation: 0.0,
+                  ),
+                ),
+                Expanded(
+                  child: RaisedButton(
+                    child: Text("复制"),
+                    onPressed: () {
+                      Clipboard.setData(ClipboardData(text: _url));
+                      MyToastUtils("已复制链接到剪切板");
+                    },
+                    splashColor: Colors.white,
+                    color: Colors.pinkAccent,
+                    elevation: 0.0,
+                  ),
+                ),
+                Expanded(
+                  child: RaisedButton(
+                    child: Text("内跳"),
+                    onPressed: () {
+                      lunchList(context, pageBean.dataLists[index]["name"],
+                          pageBean.dataLists[index]);
+                    },
+                    splashColor: Colors.greenAccent,
+                    color: Colors.amberAccent,
+                    elevation: 0.0,
+                  ),
+                ),
+              ],
+            ),
           ],
         ),
       ),
     );
+  }
+
+  _launchURL(url) async {
+    print("跳转地址：" + url);
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
   }
 
   /**
