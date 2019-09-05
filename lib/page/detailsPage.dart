34dca7976ee6bf4b3bc69871651463c1bca529b2
叶阳 <hbyeyang@yeah.net>
2019-09-05 12:31:30 +0800

    添加自定义标题，bar,原页面刷新功能

--- lib/page/detailsPage.dart
+++ lib/page/detailsPage.dart
@@ -1,11 +1,22 @@
 import 'package:demo_test/bean/detailsBean.dart';
+import 'package:demo_test/util/utils.dart';
+import 'package:demo_test/view/baseTitleBar.dart';
+import 'package:demo_test/view/baseViewBar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 
-class DeatailsPage extends StatelessWidget {
-  DetailsBean _deatailsBean;
-  String _url;
+class DeatailsPage extends StatefulWidget {
+  DetailsBean deatailsBean;
+  String netUrl;
 
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return WebPageState();
+  }
+}
+
+class WebPageState extends State<DeatailsPage> {
 //  @override
 //  Widget build(BuildContext context) {
   // TODO: implement build
@@ -23,16 +34,17 @@ class DeatailsPage extends StatelessWidget {
 //    );
   @override
   Widget build(BuildContext context) {
-    _deatailsBean = ModalRoute.of(context).settings.arguments;
-    if (!_deatailsBean.details['url'].startsWith("http")) {
-      _url = "http://" + _deatailsBean.details['url'];
+    DetailsBean deatailsBean = ModalRoute.of(context).settings.arguments;
+    String netUrl;
+    if (!deatailsBean.details['url'].startsWith("http")) {
+      netUrl = "http://" + deatailsBean.details['url'];
     }
 //    _url = "https://juejin.im/post/5c452bac6fb9a049af6d919a#heading-3";
-    print("页面地址：" + _url);
+    print("页面地址：" + netUrl);
     List<Widget> titleContent = [];
     titleContent.add(new Text(
 //      "详情页面",
-      _deatailsBean.title,
+      deatailsBean.title,
       style: new TextStyle(color: Colors.white),
     ));
     titleContent.add(new Container(width: 50.0));
@@ -42,14 +54,29 @@ class DeatailsPage extends StatelessWidget {
 //      url: "https://flutter.dev/docs/development/tools/hot-reload",
 //      url: "http://t.channel.45xie.com/manhua/home.html?appid=E9121AF6F5633600943AA69DB667CFB9",
 //      url: "http://letupower.com",
-      url: _url,
+      url: netUrl,
       // 登录的URL
-      appBar: new AppBar(
-        title: new Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: titleContent,
+//      appBar: new AppBar(
+//        title: new Row(
+//          mainAxisAlignment: MainAxisAlignment.center,
+//          children: titleContent,
+//        ),
+//        iconTheme: new IconThemeData(color: Colors.white),
+//      ),
+
+      appBar: BaseViewBar(
+        childView: BaseTitleBar(
+          "详情页面",
+          leftIcon: Icons.arrow_back,
+          rightText: "刷新",
+          rightClick: () {
+            MyToastUtils("刷新页面");
+            setState(() {
+              FlutterWebviewPlugin().reload();
+            });
+          },
         ),
-        iconTheme: new IconThemeData(color: Colors.white),
+        preferredSize: Size.fromHeight(50.0),
       ),
       withZoom: true,
       // 允许网页缩放
--- lib/page/standbyPage.dart
+++ lib/page/standbyPage.dart
@@ -1,38 +1,64 @@
 import 'package:demo_test/bean/standbyBean.dart';
+import 'package:demo_test/util/utils.dart';
+import 'package:demo_test/view/baseTitleBar.dart';
+import 'package:demo_test/view/baseViewBar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 
-class StandbyPage extends StatelessWidget {
-  StandbyBean _standbyBean;
-  String _title;
-  String _url;
+class StandbyPage extends StatefulWidget {
+  StandbyBean standbyBean;
+  String titleText;
+  String netUrl;
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return WebPageState();
+  }
+}
+
+class WebPageState extends State<StandbyPage> {
   @override
   Widget build(BuildContext context) {
-    _standbyBean = ModalRoute.of(context).settings.arguments;
-    _title = _standbyBean.title;
-    _url = _standbyBean.url;
-    if (!_standbyBean.url.startsWith("http://")) {
-      _url = "http://" + _standbyBean.url;
+    StandbyBean standbyBean = ModalRoute.of(context).settings.arguments;
+    String titleText = standbyBean.title;
+    String netUrl = standbyBean.url;
+    if (!standbyBean.url.startsWith("http://")) {
+      netUrl = "http://" + standbyBean.url;
     }
-    print("页面地址" + _url);
+    print("页面地址" + netUrl);
     return new WebviewScaffold(
-      url: _url,
+      url: netUrl,
       // 登录的URL
-      appBar: new AppBar(
-        leading: IconButton(
-            icon: Icon(
-              Icons.arrow_back,
-              color: Colors.white,
-            ),
-            onPressed: () {
-              Navigator.pop(context);
-            }),
-        title: Text(
-          _title,
-          style: TextStyle(fontSize: 16.0),
-          maxLines: 1,
-          overflow: TextOverflow.ellipsis,
+//      appBar: new AppBar(
+//        leading: IconButton(
+//            icon: Icon(
+//              Icons.arrow_back,
+//              color: Colors.white,
+//            ),
+//            onPressed: () {
+//              Navigator.pop(context);
+//            }),
+//        title: Text(
+//          _title,
+//          style: TextStyle(fontSize: 16.0),
+//          maxLines: 1,
+//          overflow: TextOverflow.ellipsis,
+//        ),
+//          ),
+      appBar: BaseViewBar(
+        childView: BaseTitleBar(
+          titleText,
+          leftIcon: Icons.arrow_back,
+          rightText: "刷新",
+          rightClick: () {
+            MyToastUtils("刷新页面");
+            setState(() {
+              FlutterWebviewPlugin().reload();
+            });
+          },
         ),
+        preferredSize: Size.fromHeight(50.0),
       ),
       withZoom: true,
       // 允许网页缩放
